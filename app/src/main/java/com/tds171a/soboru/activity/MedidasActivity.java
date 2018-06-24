9153b6c7d1431ebed6f3253a8ea3a7256479017b
Diogo S. Moraes <srto.dih@gmail.com>
2018-06-24 15:16:58 -0300

    Usando JSONObject e JsonObjectRequest

--- app/src/main/java/com/tds171a/soboru/activity/MedidasActivity.java
+++ app/src/main/java/com/tds171a/soboru/activity/MedidasActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.NetworkResponse;
@@ -36,7 +37,9 @@ import org.json.JSONObject;
 
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.annotation.Nullable;
 
@@ -53,8 +56,6 @@ public class MedidasActivity  extends Activity{
         txt_id_medida = findViewById(R.id.txt_id_medida);
         txt_nome_medida = findViewById(R.id.txt_nome_medida);
         txt_abre_medida = findViewById(R.id.txt_abre_medida);
-
-
     }
 
     /*public  void gerarJSON(View v){
@@ -89,19 +90,16 @@ public class MedidasActivity  extends Activity{
         txt_abre_medida.setText(medida.getAbreviacao());
     }
 
-
-
     public void inserirMedida(View v){
-
-        String url = "http://soboruapi.azurewebsites.net/api/Medidas/";
+        //String url = "http://soboruapi.azurewebsites.net/api/Medidas/";
         respostaJSONtxt = findViewById(R.id.respostaJSONtxt);
         edit_nome = findViewById(R.id.edit_nome);
         edit_abre = findViewById(R.id.edit_abre);
 
 
-        final String stringBody = "{  \"Nome\": \" " + edit_nome.getText() +"\",  \"Abreviacao\": \""+ edit_abre.getText() +"\",  \"CreatedAt\": \"2018-06-20\",  \"UpdatedAt\": \"2018-06-20\" }";
+        //final String stringBody = "{  \"Nome\": \" " + edit_nome.getText() +"\",  \"Abreviacao\": \""+ edit_abre.getText() +"\",  \"CreatedAt\": \"2018-06-20\",  \"UpdatedAt\": \"2018-06-20\" }";
 
-        StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+        /*StringRequest request = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
                 Log.i("VOLLEY", response);
@@ -136,8 +134,55 @@ public class MedidasActivity  extends Activity{
                 }
                 return Response.success(responseString, HttpHeaderParser.parseCacheHeaders(response));
             }
-        };
-        MySingleton.getInstance(this).addToRequest(stringRequest);
+        };*/
+
+//        Toast.makeText(this, "Fazendo chamada", Toast.LENGTH_SHORT).show();
+
+        String url = "http://10.0.2.2:5000/api/medida/";
+
+//        String stringBody = "{  \"nome\": \" " + edit_nome.getText() +"\",  \"abreviacao\": \""+ edit_abre.getText() +"\" }";
+
+        Map<String, String> mapString = new HashMap<String, String>();
+
+        mapString.put("nome", edit_nome.getText().toString());
+        mapString.put("abreviacao", edit_abre.getText().toString());
+
+        Gson gson = new Gson();
+        String mapJson = gson.toJson(mapString);
+
+        JSONObject jObj = null;
+        try {
+//            jObj = new JSONObject(stringBody)
+              jObj = new JSONObject(mapJson);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+//        jObj = new JSONObject(mapString);
+
+        JsonObjectRequest request = new JsonObjectRequest(url, jObj,
+            new Response.Listener<JSONObject>() {
+                @Override
+                public void onResponse(JSONObject response) {
+                    try {
+                        VolleyLog.v("Response:%n %s", response.toString(4));
+//                        Toast.makeText(MedidasActivity.this, "Response: " + response.toString(4), Toast.LENGTH_SHORT).show();
+
+                        Gson gson = new Gson();
+                        Medida medida = gson.fromJson(response.toString(), Medida.class);
+                        Toast.makeText(MedidasActivity.this, medida.getNome(), Toast.LENGTH_SHORT).show();
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    VolleyLog.e("Error: ", error.getMessage());
+                    Toast.makeText(MedidasActivity.this, "Error: " + error.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            });
+
+        MySingleton.getInstance(this).addToRequest(request);
 
     }
 }
